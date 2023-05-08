bdce8330328cc42b040d8bb902a978aa975011cd
anshulghogre4 <anshulghogre4@gmail.com>
2023-05-08 23:16:46 +0530

    added cors config in the server side

--- API/Program.cs
+++ API/Program.cs
@@ -16,14 +16,18 @@ builder.Services.AddDbContext<DataContext>(opt =>
 
 });
 
+builder.Services.AddCors();
+
 
 
 
 
 
 var app = builder.Build();
-
+//middlewares
 // Configure the HTTP request pipeline.
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 
 app.MapControllers();
