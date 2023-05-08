0ba35717d4f56eebd9ece50cc6e07823e758f4b6
anshulghogre4 <anshulghogre4@gmail.com>
2023-05-08 18:35:38 +0530

    done the basic databse connection with sqlite and tested user controller with postman

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        // in C# we use pascal casing
+        [Key]
+        public int Id { get; set; }
+        // in C# int are not optional that's why we have to provide value to the int
+        public string UserName { get; set; }
+        // while in string it can be optional means string can be null aswell
+        //currently we disabled the nullable
+
+
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,31 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// we can add service here anywhere above "var app = builder.Build();" this line
+
+
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    //here we're configuring the connection string and making the name of  connection string name as "defaultConnection"
+
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+
+});
+
+
+builder.Services.AddControllers();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+
+app.MapControllers();
+
+app.Run();
