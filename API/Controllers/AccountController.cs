83f41fc3746ef69c13137ea1c15eda345abdf22e
anshulghogre4 <anshulghogre4@gmail.com>
2023-05-09 17:47:02 +0530

    added DTOs and using data annotations there only

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,8 +3,10 @@
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -20,15 +22,18 @@ namespace API.Controllers
 
         [HttpPost("register")] // Post /api/account/register
 
-        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
+
+            if (await UserExists(registerDto.UserName)) return BadRequest("Username already exists");
+
             using var hmac = new HMACSHA512();
             // once we're done with the class and to dispose or to get collected by garbage collector the class for the same we use "using" keyword
 
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.UserName.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
 
@@ -36,5 +41,15 @@ namespace API.Controllers
             await _context.SaveChangesAsync();
             return user;
         }
+
+        private async Task<bool> UserExists(string username)
+        {
+
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+
+        }
+
+
+
     }
 }
