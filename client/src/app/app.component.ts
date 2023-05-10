2b15941cb5a1eacb0f25ec3517b1c89c185530d3
anshulghogre4 <anshulghogre4@gmail.com>
2023-05-10 13:30:26 +0530

    Token Service Created

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,25 +4,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-
-
-
 export class AppComponent implements OnInit {
   title = 'Dating App';
-  users : any;
+  users: any;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5000/api/users').subscribe({
-      next: response  => this.users = response,
-      error: error => console.log(error),
-      complete : () => console.log('Request Completed')
-    })
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: (response) => (this.users = response),
+      error: (error) => console.log(error),
+      complete: () => console.log('Request Completed'),
+    });
   }
-
-
-  
 }
